da09997a9b0d72e5801fe33a0d676d9b0319635e
Abishek Anthony <54862087+abishekanthony07@users.noreply.github.com>
2025-04-17 06:24:27 +0200

    initial setup KMP with https://kmp.jetbrains.com/

---
+++ composeApp/src/desktopMain/kotlin/com/abishekanthony/kmp/main.kt
@@ -0,0 +1,13 @@
+package com.abishekanthony.kmp
+
+import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.application
+
+fun main() = application {
+    Window(
+        onCloseRequest = ::exitApplication,
+        title = "KotlinProject",
+    ) {
+        App()
+    }
+}
---
+++ shared/src/commonMain/kotlin/com/abishekanthony/kmp/Constants.kt
@@ -0,0 +1,3 @@
+package com.abishekanthony.kmp
+
+const val SERVER_PORT = 8080
